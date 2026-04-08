e5dc237412202a67564ad9806a7fefbc3f318a5b
Federico Guillermo Gravina <gravinadavilafederico@gmail.com>
2026-04-08 06:57:13 -0300

    Update app.js

--- sullair_pricing_front_mod/app.js
+++ sullair_pricing_front_mod/app.js
@@ -1,4 +1,4 @@
-const API_BASE = window.__API_BASE__ || "http://127.0.0.1:8000";
+const API_BASE = "https://pricing-optimization-api.onrender.com";
 const LLM_TOKEN_KEY = "pricing_engine_llm_token";
 
 const el = (id) => document.getElementById(id);
